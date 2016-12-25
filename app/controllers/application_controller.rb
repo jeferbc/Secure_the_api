fab7d93bdb47b48bcba7fada80a6d3968d5394eb
jeferbc <jeffe.bernal@gmail.com>
2016-12-24 19:03:33 -0500

    Add secure

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,16 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+  
+  
+  def authenticate
+    user = User.find_by(email: request.headers['HTTP_X_USER_EMAIL'])
+    acces = user ? request.headers['X-API-TOKEN'] == user.api_token : false
+      
+    unless acces
+      render json: { errors: "Acceso denegado" }, status: 401
+    end
+  end
+  
+end
